87d34102b1160e9930ae2e465572b9a4bbc76671
Tatsiana Kuryla <tatianakuryla@gmail.com>
2025-11-24 10:36:32 +0100

    config: add tailwind config, set up project fonts and colors

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,40 @@
-import type { Metadata } from 'next';
 import './globals.css';
-import type { ReactNode } from 'react';
+import { Roboto, Paytone_One } from 'next/font/google';
+
+import type { Metadata } from 'next';
+import type { ReactNode, JSX } from 'react';
+
+import { Header } from '@/components/layout/Header/Header';
 
 export const metadata: Metadata = {
-  title: 'Casino landing',
-  description: 'Landing page for casino test task',
+  title: 'Ripper Casino',
+  description:
+    'Ripper Casino offers the best online casinos in Australia - generous bonuses, popular slots, live dealers and secure payment methods. Discover top offers today.',
 };
 
-export default function RootLayout({ children }: { children: ReactNode }) {
+const roboto = Roboto({
+  subsets: ['latin'],
+  weight: '500',
+  variable: '--font-roboto',
+});
+
+const paytoneOne = Paytone_One({
+  subsets: ['latin'],
+  weight: '400',
+  variable: '--font-paytone-one',
+});
+
+export default function RootLayout({
+  children,
+}: {
+  children: ReactNode;
+}): JSX.Element {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`${roboto.variable} ${paytoneOne.variable}`}>
+      <body className="font-sans bg-bg-main text-text-main my-0 mx-auto">
+        <Header />
+        {children}
+      </body>
     </html>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  content: ['./app/**/*.{ts,tsx,js,jsx}', './components/**/*.{ts,tsx,js,jsx}'],
+  theme: {
+    extend: {},
+  },
+};
