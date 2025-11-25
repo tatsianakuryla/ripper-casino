cc6810a51f5b64798cd345f48dd0dc3489b460ad
Tatsiana Kuryla <tatianakuryla@gmail.com>
2025-11-25 12:37:29 +0100

    feat: implement shuffleArray helper

---
+++ shared/helpers/shuffle.ts
@@ -0,0 +1,8 @@
+export function shuffleArray<T>(offers: T[]): T[] {
+  const result = [...offers];
+  for (let index = result.length - 1; index > 0; index -= 1) {
+    const randomIndex = Math.floor(Math.random() * (index + 1));
+    [result[index], result[randomIndex]] = [result[randomIndex], result[index]];
+  }
+  return result;
+}
